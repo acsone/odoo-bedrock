7d9bd77710149553cf7b754ed2c9ab1bb45c3d0f
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-09-30 12:42:52 +0200

    Default PGPORT

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -12,7 +12,7 @@ confd -log-level=warn -onetime -backend ${CONFD_BACKEND:-env} ${CONFD_OPTS:-}
 
 # TODO this could (should?) be sourced from file(s) under confd control
 export PGHOST=${DB_HOST}
-export PGPORT=${DB_PORT:-}
+export PGPORT=${DB_PORT:-5432}
 export PGUSER=${DB_USER}
 export PGPASSWORD=${DB_PASSWORD}
 export PGDATABASE=${DB_NAME}
