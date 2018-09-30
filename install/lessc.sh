6df2367fea113bdad6fca30217f5ee5e5689c1ba
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-09-30 11:06:28 +0200

    Separate install script for lessc

--- install/lessc.sh
+++ install/lessc.sh
@@ -2,6 +2,4 @@
 set -Eeuxo pipefail
 
 apt-get install -y --no-install-recommends \
-  node-clean-css \
-  xfonts-75dpi \
-  xfonts-base
+  node-less
--- install/odoo-deps-9.0.sh
+++ install/odoo-deps-9.0.sh
@@ -3,6 +3,5 @@ set -Eeuxo pipefail
 
 apt-get install -y --no-install-recommends \
   node-clean-css \
-  node-less \
   xfonts-75dpi \
   xfonts-base
