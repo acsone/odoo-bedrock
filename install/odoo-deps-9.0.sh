d2102ef69d3235ca378dfe021af22f0c9464cbb3
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-09-30 11:02:58 +0200

    Remove optional dependencies
    antiword, ghostscript, graphviz, poppler-utils

--- install/odoo-deps-9.0.sh
+++ install/odoo-deps-9.0.sh
@@ -2,11 +2,7 @@
 set -Eeuxo pipefail
 
 apt-get install -y --no-install-recommends \
-  antiword \
-  ghostscript \
-  graphviz \
   node-clean-css \
   node-less \
-  poppler-utils \
   xfonts-75dpi \
   xfonts-base
