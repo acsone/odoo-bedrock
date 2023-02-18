5e1af1b0408c56b4178480643661af6243ea51c9
hparfr <raphael.reverdy@akretion.com>
2023-02-18 01:04:01 +0100

    remove confd
    the choice of confd or another tool or no tool
    should be done elsewhere

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -8,14 +8,6 @@ USER_ID=${LOCAL_USER_ID:-999}
 
 id -u odoo &> /dev/null || useradd --shell /bin/bash -u $USER_ID -o -c "" -m odoo
 
-confd -log-level=warn -onetime -backend ${CONFD_BACKEND:-env} ${CONFD_OPTS:-}
-
-# TODO this could (should?) be sourced from file(s) under confd control
-export PGHOST=${DB_HOST}
-export PGPORT=${DB_PORT:-5432}
-export PGUSER=${DB_USER}
-export PGPASSWORD=${DB_PASSWORD}
-export PGDATABASE=${DB_NAME}
 
 mkdir -p /data/odoo/{addons,filestore,sessions}
 if [ ! "$(stat -c '%U' /data/odoo)" = "odoo" ]; then
