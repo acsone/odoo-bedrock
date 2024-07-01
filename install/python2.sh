dfe53be6abbcdb4b4df00913bb4b29aea2374340
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2024-07-01 15:39:45 +0200

    Use jammy for python 2 images (odoo <= 10)

--- install/python2.sh
+++ install/python2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -Eeuxo pipefail
 
-apt-get install -y --no-install-recommends python-virtualenv
+apt-get install -y --no-install-recommends python2 python-pip
+python2 -m pip install virtualenv
+
