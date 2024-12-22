68a50db40fd57ce117ea8cc60b5ee8bcd9c0f4f4
Slavek Miskovec <spacimek@gmail.com>
2024-12-22 17:33:02 +0100

    Update version of kw to 1.0
    Just to be aligned with the latest updates, using v1.0 of kwkhtmltopdf_client

--- install/wkhtmltopdf.sh
+++ install/wkhtmltopdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
-curl -o /usr/local/bin/wkhtmltopdf -SL https://github.com/acsone/kwkhtmltopdf/releases/download/0.9.0/kwkhtmltopdf_client
-echo "49b609de7e7964b65c2021e71f651eb1c5f76112 /usr/local/bin/wkhtmltopdf" | sha1sum -c -
+curl -o /usr/local/bin/wkhtmltopdf -SL https://github.com/acsone/kwkhtmltopdf/releases/download/1.0/kwkhtmltopdf_client
+echo "4856c49a1a20a949548a2535ac98de64873bd167 /usr/local/bin/wkhtmltopdf" | sha1sum -c -
 chmod +x /usr/local/bin/wkhtmltopdf
